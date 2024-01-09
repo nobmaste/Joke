de704c6673c751d2c7c15ecb0faff50cbf513035
nobmaste <maleming031621@163.com>
2024-01-09 22:21:07 +0800

    实时帧测试

--- page/main/main.js
+++ page/main/main.js
@@ -12,5 +12,21 @@ Page({
     }
   },
 
+  takePhoto() {
+      const ctx = wx.createCameraContext()
+      ctx.takePhoto({
+        quality: 'high',
+        success: (res) => {
+          this.setData({
+            src: res.tempImagePath
+          })
+        }
+      })
+    },
+    error(e) {
+      console.log(e.detail)
+    }
+ 
+
 
 });
