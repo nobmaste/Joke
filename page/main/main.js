c1d9823c1f94ce217d3ba8d0491f969ec58f7a8e
nobmaste <maleming031621@163.com>
2024-01-10 23:06:44 +0800

    log frame.data为空，真机预览卡死

--- page/main/main.js
+++ page/main/main.js
@@ -1,52 +1,19 @@
 var log = require('../../log/log.js');// 引用上面的log.js文件
-// pages/index/index.js
 
 Page({
   data: {
     cameraContext: null,
-    isDataArrayBuffer: false,
-    frameWidth: 0,
-    frameHeight: 0,
-    imageDataUrl: '',
+   
   },
 
   onLoad() {
     // 获取摄像头上下文对象
-    this.data.cameraContext = wx.createCameraContext();
-
-    // 注册 onCameraFrame 事件
-    this.data.cameraContext.onCameraFrame(this.onCameraFrame);
+    const cameraContext=wx.createCameraContext()
+    const listener = cameraContext.onCameraFrame((frame)=>{
+      console.log('Received camera frame:'),
+      console.log(frame.data)
+    })
+    listener.start()
   },
-
-  onCameraFrame(frame) {
-    // 判断帧数据是否为 ArrayBuffer 类型
-    this.data.isDataArrayBuffer = frame.data instanceof ArrayBuffer;
-
-    // 更新帧的相关信息
-    this.setData({
-      frameWidth: frame.width,
-      frameHeight: frame.height,
-    });
-
-    // 打印帧的相关信息
-    console.log('Received camera frame:');
-    console.log('Width:', frame.width);
-    console.log('Height:', frame.height);
-    console.log('Data is ArrayBuffer:', this.data.isDataArrayBuffer);
-
-    // 如果帧数据是 ArrayBuffer 类型，你可以进一步处理数据，例如将其转换为图片进行展示
-    if (this.data.isDataArrayBuffer) {
-      this.data.imageDataUrl = this.arrayBufferToDataURL(frame.data, frame.width, frame.height);
-      console.log('Image Data URL:', this.data.imageDataUrl);
-      // 在这里你可以使用 imageDataUrl 进行进一步的展示或处理
-    }
-  },
-
-  // 将 ArrayBuffer 转换为 Data URL（Base64 编码）
-  arrayBufferToDataURL(arrayBuffer, width, height) {
-    const blob = new Blob([arrayBuffer], { type: 'image/jpeg' });
-    const urlCreator = window.URL || window.webkitURL;
-    const imageUrl = urlCreator.createObjectURL(blob);
-    return imageUrl;
-  },
+ 
 });
